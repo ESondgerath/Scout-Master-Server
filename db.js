44be7599f32c58e5354c150ce5a87754e662feba
Evan Sondgerath <esondgerath@gmail.com>
2018-07-31 13:40:22 -0400

    Heroku Deployment Changes

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DBNAME, process.env.PGUSER, process.env.PGPASS, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    port: 5432,
-    host: process.env.PGHOST
 });
 
 sequelize.authenticate().then(
