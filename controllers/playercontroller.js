8edd1df4767d4759c5210880a2746d05aad20201
Jack Demaree <jackdemaree@Jacks-MacBook-Pro.local>
2018-07-31 21:19:05 -0400

    final tuesday night

--- controllers/playercontroller.js
+++ controllers/playercontroller.js
@@ -6,102 +6,102 @@ var Search = require('../routes/search-players');
 
 router.get('/', function(req, res) {
 
-    Player
-    .getAll()
-    .then(
-      function findAllSuccess(data) {
-          res.json(data);
-      },
-      function findAllError(err) {
-          res.send(500, err.message);
-      }
-  );
+   Player
+   .getAll()
+   .then(
+     function findAllSuccess(data) {
+         res.json(data);
+     },
+     function findAllError(err) {
+         res.send(500, err.message);
+     }
+ );
 })
 
 // router.get(Search)
 
 router.post('/create', function(req, res) {
 
-    Player.createPlayer(req)
-    .then(
-        function createSuccess(player) {
-            res.json({
-                player: player
-            })
-        },
-        function createError(err) {
-            res.send(500, err.message)
-        }
-    )
+   Player.createPlayer(req)
+   .then(
+       function createSuccess(player) {
+           res.json({
+               player: player
+           })
+       },
+       function createError(err) {
+           res.send(500, err.message)
+       }
+   )
 })
 
 router.put('/edit/:id', function(req, res) {
 
-    Player.updatePlayer(req, req.params.id)
-        
-        .then(
-            function updateSuccess() {
-                res.json({
-                    playername: req.body.player.playername,
-                    position: req.body.player.position,
-                    role: req.body.player.role,
-                    technical: req.body.player.technical,
-                    mental: req.body.player.mental,
-                    physical: req.body.player.physical
-                });            
-            },
-            function updateError(err){
-                res.send(500, err.message);
-            }
-        )
+   Player.updatePlayer(req, req.params.id)
+       
+       .then(
+           function updateSuccess() {
+               res.json({
+                   playername: req.body.playername,
+                   position: req.body.position,
+                   role: req.body.role,
+                   technical: req.body.technical,
+                   mental: req.body.mental,
+                   physical: req.body.physical
+               });            
+           },
+           function updateError(err){
+               res.send(500, err.message);
+           }
+       )
 });
 
-router.put('/edit', (req, res) => {
+// router.put('/edit', (req, res) => {
 
-    Player.updatePlayer(req, res)
-        .then(
-            function updateSuccess() {
-                res.json({
-                    playername: req.body.player.playername,
-                    position: req.body.player.position,
-                    role: req.body.player.role,
-                    technical: req.body.player.technical,
-                    mental: req.body.player.mental,
-                    physical: req.body.player.physical
-                });
-            },
-            function updateError(err) {
-                res.send(500, err.message);
-            }
-        )
-});
+//     Player.updatePlayer(req, res)
+//         .then(
+//             function updateSuccess() {
+//                 res.json({
+//                     playername: req.body.player.playername,
+//                     position: req.body.player.position,
+//                     role: req.body.player.role,
+//                     technical: req.body.player.technical,
+//                     mental: req.body.player.mental,
+//                     physical: req.body.player.physical
+//                 });
+//             },
+//             function updateError(err) {
+//                 res.send(500, err.message);
+//             }
+//         )
+// });
 
 router.get('/details/:id', function(req, res) {
-    var id = req.params.id;
+   var id = req.params.id;
 
-    Player.getOnePlayer(req, id)
-        .then(
-            function findOneSuccess(data) {
-                res.json(data);
-            },
-            function findOneError(err) {
-                res.send(500, err.message);
-            }
-        );
+   Player.getOnePlayer(req, id)
+       .then(
+           function findOneSuccess(data) {
+               res.json(data);
+           },
+           function findOneError(err) {
+               res.send(500, err.message);
+           }
+       );
 });
 
 router.delete('/deleteplayer/:id', function(req, res) {
-    var id = req.params.id;
+   var id = req.params.id;
 
-    Player.deletePlayer(req, id)
-        .then(
-            function deleteSuccess(data) {
-                res.send("Player Deleted");
-            },
-            function deleteError(err) {
-                res.send(500, err.message);
-            }
-        )
+   Player.deletePlayer(req, id)
+       .then(
+           function deleteSuccess(data) {
+               res.send("Player Deleted");
+           },
+           function deleteError(err) {
+               res.send(500, err.message);
+           }
+       )
 })
 
 module.exports = router;
--- services/playerServices.js
+++ services/playerServices.js
@@ -3,28 +3,28 @@ const player = sequelize.import('../models/player')
 
 
 exports.getAll = function(){
-    return player.findAll({
+   return player.findAll({
 
-    })
+   })
 }
 
 exports.getOnePlayer = function(req, id){
-    return player.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
+   return player.findOne({
+       where: {
+           id: req.params.id
+       }
+   })
 }
 
 exports.createPlayer = function(req){
-    return player.create({
-        playername: req.body.playername,
-        position: req.body.position,
-        role: req.body.role,
-        technical: req.body.technical,
-        mental: req.body.mental,
-        physical: req.body.physical
-    })
+   return player.create({
+       playername: req.body.playername,
+       position: req.body.position,
+       role: req.body.role,
+       technical: req.body.technical,
+       mental: req.body.mental,
+       physical: req.body.physical
+   })
 }
 
 // exports.editPlayer = function(req, id){
@@ -40,19 +40,19 @@ exports.createPlayer = function(req){
 // }
 
 exports.updatePlayer = function(req, res) {
-    return player.update({
-        playername: req.body.player.playername,
-        position: req.body.player.position,
-        role: req.body.player.role,
-        technical: req.body.player.technical,
-        mental: req.body.player.mental,
-        physical: req.body.player.physical
-    },
+   return player.update({
+       playername: req.body.playername,
+       position: req.body.position,
+       role: req.body.role,
+       technical: req.body.technical,
+       mental: req.body.mental,
+       physical: req.body.physical
+   },
 {where: {id: req.params.id}})
 }
 
 exports.deletePlayer = function(req){
-    return player.destroy({
-        where:{id: req.params.id}
-    })
+   return player.destroy({
+       where:{id: req.params.id}
+   })
 }
